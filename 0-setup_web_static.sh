455fa29d678ea07d4c679f8cacbcb7a38cf4af15
keen-ivan <i.mucyo@alustudent.com>
2024-10-04 21:10:49 +0200

    all tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Setting up web servers for the web_static deployment
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
